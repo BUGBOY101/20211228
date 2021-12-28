925596994f3d6e7057312c3a99b9dc9a4073054a
BUG <2251093140@qq.com>
2021-12-28 22:46:33 +0800

    添加项目文件。

---
+++ 1228.cpp
@@ -0,0 +1,10 @@
+#define	_CRT_SECURE_NO_WARNINGS 1
+
+//ÊýËþ
+// 
+//for (i = 1; i <= n; i++) {
+//	dp[n][i] = a[n][i];
+//}
+//for (i = n - 1; i >= 1; i--) {
+//	dp[i][j] += max(dp[i + 1][j], dp[i + 1][j + 1]);
+//}
